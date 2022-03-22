b359f33b299f159e5c177f653d5824b7ba81c0af
Keith Nguyen <minhnhakeith@gmail.com>
2022-03-22 21:45:59 +0700

    Finished homework table lat-long

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
   </div>
 </template>
@@ -17,7 +17,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* color: #2c3e50; */
+  background-color: #222222;
+  width: 100%;
+  height: 100vh
 }
 </style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,64 +1,225 @@
 <template>
-    <div>
-        <div>
-            <p>Coordinates</p>
-            <button @click="addOneMoreRow()">+</button>
-            <button @click="addToArray()">Display</button>
+  <div class="table-container">
+    <div class="table-coordinate mt-5">
+      <p class="table-coordinate__title">Coordinate Form</p>
+      <div class="d-flex flex-wrap justify-content-start mt-3">
+        <div
+          class="
+            col-sm-12 col-md-12 col-lg-6
+            d-flex
+            flex-wrap
+            justify-content-between
+          "
+        >
+          <div class="form-group col-12 col-sm-6 col-md-6 col-lg-3 lat-input">
+            <label htmlFor="txtLat">Lat</label>
+            <input
+              type="text"
+              class="form-control"
+              required
+              id="txtLat"
+              placeholder="Enter lat. Ex: 9.567941"
+              v-model="coordinate.lat"
+            />
+          </div>
+          <div class="form-group col-12 col-sm-6 col-md-6 col-lg-3 long-input">
+            <label htmlFor="txtLong">Long</label>
+            <input
+              type="text"
+              class="form-control"
+              required
+              id="txtLong"
+              placeholder="Enter long. Ex: 105.7459"
+              v-model="coordinate.long"
+            />
+          </div>
+          <button class="btnAddCoor" @click="addToArray()">
+            Add coordinate
+          </button>
         </div>
-        <table>
-            <tr>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6">
+          <table class="table__coordinate">
+            <thead>
+              <tr>
                 <th>Lat</th>
                 <th>Long</th>
-                <th></th>
-            </tr>
-            <tr v-for="(i) in length" :key="i">
-                <td><input type="text" v-model="coordinate.lat" placeholder="Lat" /></td>
-                <td><input type="text" v-model="coordinate.long" placeholder="Long" /></td>
-                <td>X</td>
-            </tr>
-        </table>
-        <p>{{coordinate}}</p>
-        
-        <ul>
-            <li v-for="(coor) in coordinates" :key="coor">
-                {{coor}}
-            </li>
-        </ul>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="coor in coordinates" :key="coor.id">
+                <td>
+                  
+                  <input type="text" v-model="coor.lat"/>
+                </td>
+                <td>
+                  <input type="text" v-model="coor.long"/>
+                </td>
+                <td>
+                  <span @click="deleteOneRow(coor.id)" style="cursor: pointer">X</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+    <button class="btnAddCoor" @click="log()">
+            Log
+          </button>
+  </div>
 </template>
 
 <script>
-    export default{
-        name: "Table",
-        data(){
-            return{
-                length:1,
-                coordinate:{
-                    id: 0,
-                    lat: "",
-                    long: "",
-                },
-                coordinates: []
-                
-            }
-        },
-        methods:{
-            addOneMoreRow(){
-                this.length++
-            },
-            addToArray(){
-                console.log(this.coordinate);
-                // const arr = [...this.coordinates]
-                // arr[this.coordinates.length] = this.coordinate
-                // console.log(arr)
-                // this.coordinates=arr
-                // console.log(this.coordinates)
-                this.coordinate.lat=""
-                this.coordinate.long=""
-            },
-            deleteOneRow(index){
+export default {
+  name: "Table",
+  data() {
+    return {
+      //   length: 1,
+      coordinate: {
+        id: 0,
+        lat: "",
+        long: "",
+      },
+      coordinates: [],
+    };
+  },
+  methods: {
+    // addOneMoreRow() {
+    //   this.length++;
+    // },
+    addToArray() {
+      //   console.log(this.coordinate);
+      this.coordinate.id++;
+      const arr = [...this.coordinates];
+      arr[this.coordinates.length] = Object.assign({}, this.coordinate);
+      //   console.log(arr);
+      this.coordinates = arr;
+      //   console.log(this.coordinates);
+      this.coordinate.lat = "";
+      this.coordinate.long = "";
+    },
 
-            }
-        }
+    deleteOneRow(index) {
+      let arr = [...this.coordinates];
+      arr = arr.filter(coordinate => coordinate.id !== index)
+      this.coordinates=arr
+    },
+
+    log(){
+        console.log(this.coordinates);
     }
+  },
+};
 </script>
+
+<style scoped>
+.table-container {
+  width: 1200px;
+  margin: auto;
+  padding-top: 100px;
+}
+
+.table-coordinate div {
+  padding: 0 15px;
+  margin-bottom: 1rem;
+}
+
+.table-coordinate__title {
+  font-size: 25px;
+  letter-spacing: 2px;
+  color: white;
+  padding-left: 30px;
+  font-family: "Marcellus SC", serif;
+  font-weight: 400;
+  text-transform: uppercase;
+}
+
+.table-container .form-control {
+  position: relative;
+  width: 100%;
+  margin: 0 0 14px;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 16px;
+  line-height: 20px;
+  color: #656565;
+  background-color: transparent;
+  border: 1px dashed #ccc;
+  outline: none;
+  box-sizing: border-box;
+  height: 60px;
+}
+
+.table-container label {
+  font-size: 18px;
+  line-height: 2;
+  font-family: "Courier New", Courier, monospace;
+  font-weight: 600;
+  color: white;
+  text-transform: capitalize;
+  float: left;
+}
+
+.btnAddCoor {
+  outline: 0;
+  font-size: 12px;
+  letter-spacing: 0.15em;
+  font-weight: 600;
+  text-transform: uppercase;
+  box-sizing: border-box;
+  height: 60px;
+  border: 1px solid white;
+  padding: 20px 25px;
+  color: #fff;
+  background-color: transparent;
+  white-space: nowrap;
+  cursor: pointer;
+}
+
+.btnAddCoor:hover {
+  background-color: white;
+  color: black;
+}
+
+.table__coordinate {
+  color: white;
+  width: 80%;
+  float: right;
+  margin-top: 36px;
+}
+
+.table__coordinate tr th,
+.table__coordinate tr td {
+  border: 1px dashed #ccc;
+  text-align: center;
+  padding: 5px 0;
+}
+
+.table__coordinate tr th:not(:last-child),
+.table__coordinate tr td:not(:last-child) {
+  width: 40%;
+}
+
+.table__coordinate tr th:last-child,
+.table__coordinate tr td:last-child {
+  width: 20%;
+}
+
+.table__coordinate input{
+    color: #fff;
+  background-color: transparent;
+  border: none;
+  text-align: center;
+}
+
+.table__coordinate input:focus{
+    border: none;
+    outline: none;
+}
+
+.lat-input,
+.long-input {
+  width: 48% !important;
+  padding: 0px !important;
+}
+</style>
